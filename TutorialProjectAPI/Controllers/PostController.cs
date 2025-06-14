ab02e1badd5780c31b978802c3acbf9198578d1c
casinocathedral <214201161+casinocathedral@users.noreply.github.com>
2025-06-14 03:15:53 +0200

    Added images for posts and users

--- TutorialProjectAPI/Controllers/PostController.cs
+++ TutorialProjectAPI/Controllers/PostController.cs
@@ -40,11 +40,37 @@ namespace TutorialProjectAPI.Controllers
                     if (!_context.Users.Any(u => u.Id == postDto.UserId))
                         return BadRequest($"User with ID {postDto.UserId} does not exist.");
 
+                    Guid? imageId = null;
+
+                    // Handle image creation or reference
+                    if (!string.IsNullOrWhiteSpace(postDto.ImageBase64))
+                    {
+                        if (postDto.ImageBase64.Length > 136000)
+                            return BadRequest("Image exceeds 100kb base64 size limit.");
+
+                        var image = new ImageDB
+                        {
+                            Id = Guid.NewGuid(),
+                            Base64Image = postDto.ImageBase64
+                        };
+                        _context.Images.Add(image);
+                        _context.SaveChanges();
+                        imageId = image.Id;
+                    }
+                    else if (postDto.ImageId.HasValue)
+                    {
+                        // Check if the referenced image exists
+                        if (!_context.Images.Any(i => i.Id == postDto.ImageId.Value))
+                            return BadRequest($"Image with ID {postDto.ImageId.Value} does not exist.");
+                        imageId = postDto.ImageId.Value;
+                    }
+
                     var post = new PostDB
                     {
                         Id = Guid.NewGuid(),
                         Body = postDto.Body,
                         UserId = postDto.UserId,
+                        ImageId = imageId,
                         Replies = new List<ReplyDB>()
                     };
 
